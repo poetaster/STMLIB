479930e07d8bc2ac77f6e918246cbc6009eb0eaf
Mark Washeim <blueprint@poetaster.de>
2025-12-17 08:47:36 +0100

    Refactor: naming of inc files

--- src/stmlib_all.cpp
+++ src/stmlib_all.cpp
@@ -1,6 +1,6 @@
 // stmlib_all.cpp - Single compilation unit for STMLIB
 // Only includes the .inc files that exist in poetaster's arduinoMI port
 
-#include "stmlib/dsp/atan.cc"
-#include "stmlib/dsp/units.cc"
-#include "stmlib/utils/random.cc"
+#include "stmlib/dsp/atan.inc"
+#include "stmlib/dsp/units.inc"
+#include "stmlib/utils/random.inc"
